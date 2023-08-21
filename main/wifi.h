c87c6b20de7debeb0021a5a6b1d9167d5c967227
boyarkin.k <boyarkin.k@local.pulsar>
2023-08-21 16:50:35 +0300

    [ref] Выделил код wifi в отдельный файл

---
+++ main/wifi.h
@@ -0,0 +1,15 @@
+/*
+ * wifi.h
+ *
+ *  Created on: 21 авг. 2023 г.
+ *      Author: boyarkin.k
+ */
+
+#ifndef MAIN_WIFI_H_
+#define MAIN_WIFI_H_
+
+
+void WifiInit(void);
+
+
+#endif /* MAIN_WIFI_H_ */
